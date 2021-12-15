efa50298b900bb72c9d1b4476df8cd03ba22814a
Casey Cook <s3722194@student.rmit.edu.au>
2021-12-15 15:10:57 +1100

    dungeon started

---
+++ NoiseGame/Assets/Scripts/DungoenManger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungoenManger : MonoBehaviour
+{
+    private static DungoenManger _instance;
+
+    public static DungoenManger Instance { get { return _instance; } }
+
+    private Dictionary<string, GameObject> walls = new Dictionary<string, GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+    }
+}
